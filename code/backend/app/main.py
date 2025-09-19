bcec6278d3f01683d1838234f1d62af6c59198ec
Gregory Grimaylo <gregrimaylo@gmail.com>
2025-09-19 15:32:03 -0400

    BMP-147: added routing logic. made querying jambase API asynchronous

--- code/backend/app/main.py
+++ code/backend/app/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+import httpx
+from fastapi import FastAPI, Query
 
 app = FastAPI()
 
@@ -9,3 +10,21 @@ async def root():
         "status": "ok",
         "message": "Backend is running. Main entry point for beatmap.",
     }
+
+@app.get("/search")
+async def search(
+    city: str = Query(..., description="City to search concerts in."),
+    start_date: str = Query(..., description="Start date YYYY-MM-DD"),
+    end_date: str = Query(..., description="End date YYYY-MM-DD"),
+    provider: str = Query(..., description="Provider to search events from")
+):
+    if provider.lower() == "jambase":
+        # TODO: not sure how to send a request to jambase microservice from here
+        url = f'http://localhost:8002/jambase/search'
+        async with httpx.AsyncClient() as client:
+            response = await client.get(url, params = {
+                "city" : city,
+                "start_date" : start_date,
+                "end_date" : end_date
+            })
+        return response.json()
