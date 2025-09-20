7a868be05a6c20b0f29fcdb4b7436b3fadb6631a
Gregory Grimaylo <gregrimaylo@gmail.com>
2025-09-19 20:40:03 -0400

    BMP-147: added unit tests in test_main.py, some lines were added in init.py files to get the tests to work

---
+++ code/backend/app/__init__.py
@@ -0,0 +1,2 @@
+# had to add this line to get my tests to work in test_main, not sure why
+from . import api
--- code/backend/tests/test_main.py
+++ code/backend/tests/test_main.py
@@ -1,5 +1,11 @@
 from fastapi.testclient import TestClient
-from app.main import app
+from ..app.main import app
+from ..app.clients import jambase_client
+
+from unittest.mock import patch, AsyncMock, Mock
+import pytest
+
+from ..app.api import concerts
 
 client = TestClient(app)
 
@@ -14,3 +20,54 @@ def test_root_content():
     """Test that the root endpoint contains the correct text"""
     response = client.get("/")
     assert "beatmap" in response.text
+
+@pytest.mark.asyncio
+async def test_get_city_id():
+    mock_response = Mock()
+    mock_response.json.return_value =  {
+        "cities": [
+        {
+            "@type": "City",
+            "identifier": "jambase:123",
+            "name": "Boston",
+        }
+    ]
+    }
+    mock_response.raise_for_status = lambda: None
+
+    with patch(
+        "backend.app.clients.jambase_client.httpx.AsyncClient.get",
+        new=AsyncMock(return_value=mock_response)
+    ) as mock_get:
+        city_id = await jambase_client.get_city_id("Boston")
+        assert city_id == "jambase:123"
+        mock_get.assert_awaited_once()
+
+def test_jambase_parse_performers():
+    test_performer_list = [
+                {
+                    "@type": "MusicGroup",
+                    "name": "Turnstile",
+                    "identifier": "jambase:52143",
+                    "x-isHeadliner": True,
+                   
+                },
+                {
+                    "@type": "MusicGroup",
+                    "name": "Speed",
+                    "identifier": "jambase:6380443",
+                    "x-isHeadliner": False,
+                },
+                {
+                    "@type": "MusicGroup",
+                    "name": "Jane Remover",
+                    "identifier": "jambase:9087125",
+                    "x-isHeadliner": False,
+                }
+            ]
+    result = concerts.jambase_parse_performers(test_performer_list)
+    assert result[0] == "Turnstile"
+    assert result[1] == ["Turnstile", "Speed", "Jane Remover"]
+
+
+    
